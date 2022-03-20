40d74f3cdba62e7c343e709516ca2884b2d3ba9f
Sam Bennett <thatdevsb@protonmail.com>
2022-03-20 16:24:34 +0000

    Actually fixed what I said last: forgot git add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
+import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"about", component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
